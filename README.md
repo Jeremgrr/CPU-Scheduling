f1dc575db68eda1fa885ccfa0d50fea102b8b5e5
Jeremiah Green <jeremiahgreen@Jeremiahs-MacBook-Air.local>
2024-04-18 00:26:41 -0500

    finished 95% of task 1 FCFS scheduling, ready queue and core class added, also added readme file

---
+++ README.md
@@ -0,0 +1,7 @@
+To Clone git Repository:
+
+git clone 'https://github.com/Jeremgrr/CPU-Scheduling.git'
+
+Open directory from terminal to be able to run git commands in correct folder:
+
+cd CPU-Scheduling
--- task1.java
+++ task1.java
@@ -13,16 +13,27 @@ public class task1 extends Thread {
         dispatcher t = new dispatcher(i);
             t.start();
         }
+
+        //Probably not right
+        for (int i = 0; i < task; i++) {
+        Core t = new Core(i);
+            t.start();
+        }
+
         
     }
 }
 
+//Dispatcher Purpose
 
+//Access & loop through ready queue to find the next Tast to run
+//Decide the allotted burst for task (getMaxBurst); let CPU Core thread know burst time
+//Signal permit to a barrier blocking core thread so that the Core may proceed with its logic
+//NOTE: dispatcher thread does NOT release a permit to the Semaphore blocking the Thread, that is the Core's responsibility
 class dispatcher extends Thread{
 
     static Random r = new Random();
     static int tasks = r.nextInt(3,8);  //CHANGE to [1,25] after testing
-    static int obj = r.nextInt(3,8);    //rename
     static int count;
     static int bCount;
 
@@ -30,7 +41,7 @@ class dispatcher extends Thread{
     static Semaphore countMutex = new Semaphore(1);
     static Semaphore barrierSem = new Semaphore(0);
     static Semaphore barrierSem2 = new Semaphore(0);
-    static int[] mBurst = new int[tasks];
+    static int[] mBurst = new int[tasks];           //mBurst is the Ready Queue
     static int cBurst;
 
 
@@ -56,7 +67,7 @@ class dispatcher extends Thread{
 
     }
 
-    int[] RandBurst  = getMaxBurst();
+    static int[] RandBurst  = getMaxBurst();
 
     
 
@@ -67,19 +78,23 @@ class dispatcher extends Thread{
         System.out.println("Main thread     | Creating process thread "  + tID);
         countMutex.release();
         count++;
-        
-        
-        //when all tasks have been created now run Ready Queue
+         //when all tasks have been created now run Ready Queue
         if (count == tasks){
             System.out.println("\n--------------- Ready Queue ---------------");
-            barrierSem.release(1);       //allow 1 permit at a time so one person eats at a time
+            for (int i = 0; i < tasks; i++){
+                System.out.println("ID:" + i + ", Max Burst: " + RandBurst[i] + ", Current Burst: " + cBurst);
+                //barrierSem.release(1);
+            }     
         }
 
-       
-        barrierSem.acquireUninterruptibly();
-        //System.out.println("ID:" + tID + ", Max Burst: " + mBurst[tID] + ", Current Burst: " + cBurst);
-        System.out.println("ID:" + tID + ", Max Burst: " + RandBurst[tID] + ", Current Burst: " + cBurst);
-        barrierSem.release();
+        /*
+         * barrierSem.acquireUninterruptibly();
+         *System.out.println("ID:" + tID + ", Max Burst: " + mBurst[tID] + ", Current Burst: " + cBurst);
+         *System.out.println("ID:" + tID + ", Max Burst: " + RandBurst[tID] + ", Current Burst: " + cBurst);
+         *barrierSem.release();
+         */
+
+      
         bCount++;
 
         //if true, ready queue description has completed, move on to rest of process
@@ -93,10 +108,38 @@ class dispatcher extends Thread{
             System.out.println();
 
             barrierSem2.release(1);
-
         }
+        barrierSem.release();
 
+        
+
+    }
+
+}
+
+
+//Core Purpose
+
+//actually allow its specified task to begin running
+class Core extends Thread {
+    int tID;
+    Semaphore barrierSem2 = dispatcher.barrierSem2;
+    static int [] RandBurst = dispatcher.RandBurst;
+    static int[] mBurst = dispatcher.mBurst;
+    
+    public Core(int id){
+        tID = id;
+    }
+
+
+
+  
+
+
+    @Override
+    public void run(){
         barrierSem2.acquireUninterruptibly();
+        
         System.out.println("Dispatcher 0   | Running Process " + tID + "." );
         System.out.println("Proc. Thread " + tID +" | Using CPU 0; MB=" + RandBurst[tID] + " , CB=0, BT=" + RandBurst[tID] + " , BG:=" + RandBurst[tID]);
 
@@ -107,10 +150,12 @@ class dispatcher extends Thread{
             System.out.println("Proc. Thread " + tID +" | Using CPU 0; On Burst " + i + ".");
         }
         System.out.println();
+        
 
         barrierSem2.release();
 
+    
+
 
     }
-
 }
